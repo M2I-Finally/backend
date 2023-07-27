b23d78976d27ef7e0a035393c9d727e22854bd9a
MelanieHautekeur <mehautekeur@gmail.com>
2023-07-27 11:22:38 +0200

    Creating mapper configuration

---
+++ backend/src/main/java/fr/fin/config/BeanConfiguration.java
@@ -0,0 +1,14 @@
+package fr.fin.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+	
+	@Bean
+	public ModelMapper getMapper() {
+		return new ModelMapper();
+	}
+}
