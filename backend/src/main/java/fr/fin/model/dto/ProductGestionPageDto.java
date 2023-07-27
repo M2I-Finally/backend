45d43c64da5dbbff54be86b73b723f5307fe0b23
MelanieHautekeur <mehautekeur@gmail.com>
2023-07-27 14:01:44 +0200

    Creation of ProductGestionPageDto

---
+++ backend/src/main/java/fr/fin/model/dto/ProductGestionPageDto.java
@@ -0,0 +1,45 @@
+package fr.fin.model.dto;
+
+public class ProductGestionPageDto {
+
+	private Integer productId;
+	private String name;
+	private Double price;
+	private boolean status;
+	
+	public ProductGestionPageDto() {
+		
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+}
