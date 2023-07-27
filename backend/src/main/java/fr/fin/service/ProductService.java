644950e383f3c909b02c921f135306ed293bed87
MelanieHautekeur <mehautekeur@gmail.com>
2023-07-27 11:08:51 +0200

    Creating findAllAvailableProducts in product service

--- backend/src/main/java/fr/fin/service/ProductService.java
+++ backend/src/main/java/fr/fin/service/ProductService.java
@@ -1,8 +1,20 @@
 package fr.fin.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import fr.fin.model.entity.Product;
+import fr.fin.repository.ProductRepository;
 
 @Service
 public class ProductService {
-
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	private List<Product> getAvailableProducts() {
+		return productRepository.findAllAvailableProducts();
+	}
 }
