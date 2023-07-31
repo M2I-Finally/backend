ed2a1e9827ebd5d809db47b6493723284dc9dcb1
Etienne Verraest <verraest.etienne@gmail.com>
2023-07-31 11:26:59 +0200

    Init test folder

---
+++ backend/src/test/java/fr/fin/repository/CategoryRepositoryTests.java
@@ -0,0 +1,16 @@
+package fr.fin.repository;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class CategoryRepositoryTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
