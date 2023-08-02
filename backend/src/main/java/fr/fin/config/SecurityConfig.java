4c2674180d1cf4c655e1a00f4a77a9045a93e9c2
kt <kidztse@gmail.com>
2023-08-02 10:47:13 +0200

    AuthentificationController ok with Login & Logout, need to implement the daily sell recap after logout

--- backend/src/main/java/fr/fin/config/SecurityConfig.java
+++ backend/src/main/java/fr/fin/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	
 	@Bean
@@ -17,15 +19,20 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
+	
 	@SuppressWarnings("removal")
 	@Bean
 	  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	    return http
 	        .authorizeHttpRequests(customizer -> customizer
 	            .requestMatchers("/login").permitAll()
-	            .requestMatchers("/**").authenticated()
-	            .anyRequest().denyAll())
-	        .csrf().disable()
+	            .requestMatchers("/login/csrf").permitAll()
+	            .requestMatchers("/**").authenticated())
+	            
+	        .csrf().ignoringRequestMatchers("/login", "/logout").and()
+	        .logout((logout) -> logout
+	                .logoutSuccessUrl("/logout/success")
+	                .permitAll())
 	        .exceptionHandling(customizer -> customizer
 	            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
 	        .build();
---
+++ backend/src/main/java/fr/fin/model/dto/CrsfTokenDto.java
@@ -0,0 +1,22 @@
+package fr.fin.model.dto;
+
+public class CrsfTokenDto {
+	private String token;
+
+	public CrsfTokenDto() {
+	}
+
+	public CrsfTokenDto(String token) {
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
