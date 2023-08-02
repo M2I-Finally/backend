83cacc7bf47a2f662c1c4eba6cd2660fc48ea1dd
etienne-verraest <verraest.etienne@gmail.com>
2023-08-02 23:16:54 +0200

    Configured SecurityFilterChain with JWT

--- backend/src/main/java/fr/fin/config/BeanConfiguration.java
+++ backend/src/main/java/fr/fin/config/BeanConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
 
 	@Bean
-	public ModelMapper getMapper() {
+	ModelMapper getMapper() {
 		return new ModelMapper();
 	}
 }
--- backend/src/main/java/fr/fin/config/SecurityConfig.java
+++ backend/src/main/java/fr/fin/config/SecurityConfig.java
@@ -1,34 +1,45 @@
 package fr.fin.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthFilter;
+
+	@Autowired
+	private AuthenticationProvider authenticationProvider;
 
 	@Bean
 	PasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-
-	@SuppressWarnings("removal")
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	    return http
 	        .authorizeHttpRequests(customizer -> customizer
 	            .requestMatchers("/login").permitAll()
-	            .requestMatchers("/login/csrf").permitAll()
+	            // .requestMatchers("/login/csrf").permitAll()
 	            .requestMatchers("/**").authenticated())
-	        .csrf().ignoringRequestMatchers("/login", "/logout").and()
+	        // .csrf().ignoringRequestMatchers("/login", "/logout").and()
+	        .csrf().disable()
+	        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+	        .authenticationProvider(null).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 	        .logout(logout -> logout
 	                .logoutSuccessUrl("/logout/success")
 	                .permitAll())
