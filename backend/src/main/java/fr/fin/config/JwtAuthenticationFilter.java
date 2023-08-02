2b2ae56c6b675f4de55ba8a7026bda2df1963521
etienne-verraest <verraest.etienne@gmail.com>
2023-08-02 21:58:54 +0200

    Added JwtService class

--- backend/src/main/java/fr/fin/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/fr/fin/config/JwtAuthenticationFilter.java
@@ -2,9 +2,11 @@ package fr.fin.config;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import fr.fin.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,13 +15,27 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+	// We can intercept request and provide new data for each new request
+	// FilterChain contains the other filter from SecurityConfig
+
+	@Autowired
+	private JwtService jwtService;
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
-		// We can intercept request and provide new data for each new request
-		// FilterChain contains the other filter from SecurityConfig
+		String authHeader = request.getHeader("Authorization");
+		String jwt;
+		String username;
 
+		if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		jwt = authHeader.substring(7);
+		username = jwtService.extractUsername(jwt);
 
 
 	}
