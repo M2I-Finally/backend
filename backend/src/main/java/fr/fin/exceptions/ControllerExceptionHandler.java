9cff78eac95f7b1f05b7899a645a0963b66ef6d4
Etienne Verraest <verraest.etienne@gmail.com>
2023-08-04 11:00:29 +0200

    Added Custom Exceptions on Category Controller

--- backend/src/main/java/fr/fin/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/fr/fin/exceptions/ControllerExceptionHandler.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import fr.fin.exceptions.custom.ActionForbiddenException;
+import fr.fin.exceptions.custom.ResourceAlreadyExistsException;
 import fr.fin.exceptions.custom.ResourceNotFoundException;
+import fr.fin.exceptions.custom.ValidationErrorException;
 
 @RestControllerAdvice
 @CrossOrigin
@@ -15,7 +18,25 @@ public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	@ResponseStatus(value = HttpStatus.NOT_FOUND)
-	public ResponseError handleResourceNotFoundExceptionn(Exception ex, WebRequest request) {
+	public ResponseError handleResourceNotFoundException(Exception ex, WebRequest request) {
 		return new ResponseError(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+	}
+	
+	@ExceptionHandler(ResourceAlreadyExistsException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public ResponseError handleResourceAlreadyExistsException(Exception ex, WebRequest request) {
+		return new ResponseError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+	}
+	
+	@ExceptionHandler(ActionForbiddenException.class)
+	@ResponseStatus(value = HttpStatus.FORBIDDEN)
+	public ResponseError handleActionForbiddenException(Exception ex, WebRequest request) {
+		return new ResponseError(HttpStatus.FORBIDDEN.value(), ex.getMessage());
+	}
+	
+	@ExceptionHandler(ValidationErrorException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public ResponseError handleValidationErrorException(Exception ex, WebRequest request) {
+		return new ResponseError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
 	}
 }
