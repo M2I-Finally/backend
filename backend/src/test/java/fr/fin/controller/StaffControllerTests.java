6e02fba926569dd1af65f3cdf55763d945b7064c
HX <kidztse@gmail.com>
2023-08-17 11:24:29 +0200

    staffControllerTests updated

--- backend/src/test/java/fr/fin/controller/StaffControllerTests.java
+++ backend/src/test/java/fr/fin/controller/StaffControllerTests.java
@@ -3,7 +3,6 @@ package fr.fin.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -46,75 +45,110 @@ public class StaffControllerTests {
 
 	@Test
 	@WithMockUser(username = "admin", authorities = "ADMIN")
-	void GivenStaffsNotDeleted_WhenGetAllStaffs_ShouldReturnThem() throws Exception {
+	void givenStaffsNotDeleted_whenGetAllStaffs_shouldReturnThem() throws Exception {
 
 		// Arrange
 		List<Staff> mockList = new ArrayList<Staff>();
-		Staff staff1 = new Staff(1, "Mael","passWord123!", 0, "ADMIN", true, new Date(), new Date());
+		Staff staff1 = new Staff(1, "Mael", "passWord123!", 0, "ADMIN", true, new Date(), new Date());
 		Staff staff2 = new Staff(2, "Jordan", "passWord123!", 0, "EMPLOYEE", true, new Date(), new Date());
 		mockList.addAll(List.of(staff1, staff2));
 
 		when(staffService.getAllStaffs()).thenReturn(mockList);
 
-		MockHttpServletRequestBuilder request =
-				MockMvcRequestBuilders.get("/users");
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users");
 
 		// Execute
 		MockHttpServletResponse response = mvc.perform(request).andReturn().getResponse();
 
 		// Assert
 		String expectedResponse = """
-		[
-			{"id":1,"username":"Mael","role":"ADMIN","status":true},
-			{"id":2,"username":"Jordan","role":"EMPLOYEE","status":true}
-		]
-		""";
+				[
+					{"id":1,"username":"Mael","role":"ADMIN","status":true},
+					{"id":2,"username":"Jordan","role":"EMPLOYEE","status":true}
+				]
+				""";
 
 		assertEquals(mapper.readTree(expectedResponse), mapper.readTree(response.getContentAsString()));
 	}
 
+	@Test
+	@WithMockUser(username = "admin", authorities = "ADMIN")
+	void givenStaffIsDeleted_whenGetAllStaffs_shouldNotReturnThem() throws Exception {
+
+		// Arrange
+		List<Staff> mockList = new ArrayList<Staff>();
+		Staff staff1 = new Staff(1, "Mael", "passWord123!", 0, "ADMIN", true, new Date(), new Date());
+		Staff staff2 = new Staff(2, "Jordan", "passWord123!", 0, "EMPLOYEE", false, new Date(), new Date());
+		mockList.addAll(List.of(staff1));
+
+		when(staffService.getAllStaffs()).thenReturn(mockList);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users");
+
+		// Execute
+		MockHttpServletResponse response = mvc.perform(request).andReturn().getResponse();
+
+		// Assert
+		String expectedResponse = """
+					[{"id":1,"username":"Mael","role":"ADMIN","status":true}]
+				""";
+
+		assertEquals(mapper.readTree(expectedResponse), mapper.readTree(response.getContentAsString()));
+	}
 	
 	@Test
 	@WithMockUser(username = "admin", authorities = "ADMIN")
-	void givenStaffWithDeletedFalse_WhenGetStaffById_ShouldReturnOne() throws Exception {
-		
+	void givenStaffWithDeletedFalse_whenGetStaffById_shouldReturnOne() throws Exception {
+
 		// Arrange
-		Staff staff = new Staff(1, "Mael","passWord123!", 0, "ADMIN", true, null, null);
+		Staff staff = new Staff(1, "Mael", "passWord123!", 0, "ADMIN", true, null, null);
 		when(staffService.getStaffById(1)).thenReturn(staff);
 
-		MockHttpServletRequestBuilder request =
-				MockMvcRequestBuilders.get("/users/1");
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users/1");
 
 		// Execute
 		MockHttpServletResponse response = mvc.perform(request).andReturn().getResponse();
 
 		// Assert
 		String expectedResponse = """
-				{
-			    "id": 1,
-			    "username": "Mael",
-			    "password": null,
-			    "passwordConfirm": null,
-			    "role": "ADMIN",
-			    "status": true,
-			    "createdAt": null,
-			    "updateAt": null
-				}
-		"""
-		;
+						{
+					    "id": 1,
+					    "username": "Mael",
+					    "password": null,
+					    "passwordConfirm": null,
+					    "role": "ADMIN",
+					    "status": true,
+					    "createdAt": null,
+					    "updateAt": null
+						}
+				""";
 
 		assertEquals(mapper.readTree(expectedResponse), mapper.readTree(response.getContentAsString()));
 	}
 
 	@Test
 	@WithMockUser(username = "admin", authorities = "ADMIN")
-	void givenStaffWithDeletedTrue_WhenGetStaffById_ShouldThrowException() throws Exception {
+	void givenStaffWithDeletedTrue_whenGetStaffById_shouldNotReturn() throws Exception {
+
+		// Arrange
+		Staff staff = new Staff(1, "Mael", "passWord123!", 0, "ADMIN", false, null, null);
+		when(staffService.getStaffById(1)).thenReturn(null);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users/1");
+
+		// Execute and Assert
+		mvc.perform(request).andExpect(status().isNotFound());
+	}
+	
+	@Test
+	@WithMockUser(username = "admin", authorities = "ADMIN")
+	void givenStaffWithDeletedTrue_whenGetStaffById_shouldThrowException() throws Exception {
 
 		// Arrange
 		when(staffService.getStaffById(2)).thenReturn(null);
 
 		MockHttpServletRequestBuilder request =
-				MockMvcRequestBuilders.get("/categories/2");
+				MockMvcRequestBuilders.get("/users/2");
 
 		// Execute and assert
 		mvc.perform(request).andExpect(status().isNotFound());
@@ -122,206 +156,150 @@ public class StaffControllerTests {
 
 	@Test
 	@WithMockUser(username = "admin", authorities = "ADMIN")
-	void givenStaffDto_WhenCreateStaffWithPassword_ShouldHashPassword() throws Exception {
+	void givenStaffGestionPageDto_whenCreateStaffWithPassword_shouldHashPasswordGiven() throws Exception {
 
 		// Arrange
 		StaffGestionPageDto createStaffGestionPageDto = new StaffGestionPageDto();
 		createStaffGestionPageDto.setPassword("passWord123!");
-		
+
 		PasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		
+
 		String hashedPassword = bCryptPasswordEncoder.encode("passWord123!");
-		
+
 		// Assert
-		assertTrue(bCryptPasswordEncoder.matches(createStaffGestionPageDto.getPassword(),hashedPassword));
-		assertFalse(bCryptPasswordEncoder.matches("!Password123",hashedPassword));
+		assertTrue(bCryptPasswordEncoder.matches(createStaffGestionPageDto.getPassword(), hashedPassword));
+	}
+
+	@Test
+	@WithMockUser(username = "admin", authorities = "ADMIN")
+	void givenStaffGestionPageDto_whenCreateStaffWithPassword_shouldHashGivenPassword() throws Exception {
+
+		// Arrange
+		StaffGestionPageDto createStaffGestionPageDto = new StaffGestionPageDto();
+		createStaffGestionPageDto.setPassword("passWord123!");
+
+		PasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+		String hashedPassword = bCryptPasswordEncoder.encode("passWord123!");
+
+		// Assert
+		assertFalse(bCryptPasswordEncoder.matches("!Password123", hashedPassword));
+	}
+
+	@Test
+	@WithMockUser(username = "admin", authorities = "ADMIN")
+	void givenStaffGesionPageDtoWithBadPasswordConfirmaion_whenCreateStaff_shouldReturnValidationError() throws Exception {
+
+		// Arrange
+		StaffGestionPageDto createStaffGestionPageDto = new StaffGestionPageDto();
+		createStaffGestionPageDto.setUsername("Zoe");
+		createStaffGestionPageDto.setPassword("passWord123!");
+		createStaffGestionPageDto.setPasswordConfirm("!Password123");
+
+		String badContentJson = mapper.writeValueAsString(createStaffGestionPageDto);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/users").content(badContentJson).contentType(MediaType.APPLICATION_JSON);
+
+		// Execute and Assert
+		mvc.perform(request).andExpect(status().isBadRequest());
 	}
 
 //	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenCategoryDtoWithBadName_WhenCreateCategory_ShouldReturnValidationError() throws Exception {
+//	@WithMockUser(username = "admin", authorities = "ADMIN")
+//	void givenExistingStaffName_WhenCreateStaff_ThenReturnBadRequest() throws Exception {
 //
 //		// Arrange
-//		CreateUpdateCategoryDto createUpdateCategoryDto = new CreateUpdateCategoryDto();
-//		createUpdateCategoryDto.setName("--- Bonbons !!!0999((<!");
-//
-//		String badContentJson = mapper.writeValueAsString(createUpdateCategoryDto);
-//
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/categories").content(badContentJson).contentType(MediaType.APPLICATION_JSON);
-//
-//		// Execute and Assert
-//		mvc.perform(request).andExpect(status().isBadRequest());
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenExistingCategory_WhenCreateCategory_ThenReturnBadRequest() throws Exception {
-//
-//		// Arrange
-//		when(categoryService.checkIfCategoryExistsByName("Gateaux")).thenReturn(true);
+//		when(staffService.checkIfStaffExistsByName("Jordan")).thenReturn(true);
 //		String json = """
-//			{"id":2,"name":"Gateaux","status":true,"productCount":0}
+//				{
+//				"id":4,
+//			    "username":"Jordan",
+//			    "password":"passWord123!",
+//			    "passwordConfirm":"passWord123!",
+//			    "role": "EMPLOYEE"
+//			    }
 //		""";
 //
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/categories").content(json).contentType(MediaType.APPLICATION_JSON);
+//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/users").content(json).contentType(MediaType.APPLICATION_JSON);
 //
 //		// Execute and Assert
 //		mvc.perform(request).andExpect(status().isBadRequest());
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenACategoryId_WhenPatchStatus_ReturnUpdatedCategory() throws Exception {
-//
-//		// Arrange
-//		Category category = new Category(1, "Boissons", true, "Administrator", new Date(), false);
-//		when(categoryService.getCategoryById(1)).thenReturn(category);
-//
-//		Category categoryPatched = category;
-//		categoryPatched.setStatus(false);
-//		when(categoryService.patchCategoryStatus(1)).thenReturn(categoryPatched);
-//
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/categories/status/1");
-//
-//		// Execute and Assert
-//		mvc.perform(request).andExpect(jsonPath("$.status").value("false"));
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenAnNonExistentCategory_WhenPatchStatus_ReturnNotFoundStatus() throws Exception {
-//
-//		// Arrange
-//		when(categoryService.getCategoryById(2)).thenReturn(null);
-//
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/categories/status/2");
-//
-//		// Execute and Assert
-//		mvc.perform(request).andExpect(status().isNotFound());
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenACategoryId_WhenPatchName_ReturnUpdatedCategory() throws Exception {
-//
-//		// Arrange
-//		Category category = new Category(1, "Boissons", true, "Administrator", new Date(), false);
-//		when(categoryService.getCategoryById(1)).thenReturn(category);
-//
-//		Category categoryPatched = category;
-//		categoryPatched.setName("Boissons froides");
-//		when(categoryService.checkIfCategoryExistsByName("Boissons froides")).thenReturn(false);
-//		when(categoryService.patchCategoryName(1, "Boissons froides")).thenReturn(categoryPatched);
-//
-//		CreateUpdateCategoryDto createUpdateCategoryDto = new CreateUpdateCategoryDto();
-//		createUpdateCategoryDto.setName("Boissons froides");
-//
-//		String json = mapper.writeValueAsString(createUpdateCategoryDto);
-//
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/categories/name/1").content(json).contentType(MediaType.APPLICATION_JSON);
-//
-//		// Execute and Assert
-//		mvc.perform(request).andExpect(jsonPath("$.name").value("Boissons froides"));
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenACategoryDtoWithErrors_WhenPatchName_ReturnStatusBadRequest() throws Exception {
-//
-//		// Arrange
-//		CreateUpdateCategoryDto createUpdateCategoryDto = new CreateUpdateCategoryDto();
-//		createUpdateCategoryDto.setName("Boissons froides !!!!<<<!e!!");
-//		String badJson = mapper.writeValueAsString(createUpdateCategoryDto);
-//
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/categories/name/1").content(badJson).contentType(MediaType.APPLICATION_JSON);
-//
-//		// Execute and Assert
-//		mvc.perform(request).andExpect(status().isBadRequest());
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenACategoryExisting_WhenPatchName_ShouldReturnBadRequest() throws Exception {
-//
-//		// Arrange
-//		Category category = new Category(1, "Boissons", true, "Administrator", new Date(), false);
-//		when(categoryService.getCategoryById(1)).thenReturn(category);
-//		when(categoryService.checkIfCategoryExistsByName("Boissons froides")).thenReturn(true);
-//
-//		CreateUpdateCategoryDto createUpdateCategoryDto = new CreateUpdateCategoryDto();
-//		createUpdateCategoryDto.setName("Boissons froides");
-//		String json = mapper.writeValueAsString(createUpdateCategoryDto);
-//
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/categories/name/1").content(json).contentType(MediaType.APPLICATION_JSON);
-//
-//		// Execute and Assert
-//		mvc.perform(request).andExpect(status().isBadRequest());
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenANonExistentCategoryId_WhenPatchName_ShouldReturnNotFound() throws Exception {
-//
-//		// Arrange
-//		when(categoryService.getCategoryById(1)).thenReturn(null);
-//
-//		CreateUpdateCategoryDto createUpdateCategoryDto = new CreateUpdateCategoryDto();
-//		createUpdateCategoryDto.setName("Category");
-//		String json = mapper.writeValueAsString(createUpdateCategoryDto);
-//
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/categories/name/1").content(json).contentType(MediaType.APPLICATION_JSON);
-//
-//		// Execute and Assert
-//		mvc.perform(request).andExpect(status().isNotFound());
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenACategoryWithoutProduct_WhenDeleteCategory_ShouldReturnStatusOk() throws Exception {
-//
-//		// Arrange
-//		Category category = new Category(1, "Tartes", true, "Administrator", new Date(), false);
-//		when(categoryService.getCategoryById(1)).thenReturn(category);
-//		when(categoryService.deleteCategoryById(1)).thenReturn(true);
-//
-//		// Execute
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/categories/1");
-//
-//		// Assert
-//		mvc.perform(request).andExpect(status().isOk());
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenACategoryWithProduct_WhenDeleteCategory_ShouldReturnStatusForbidden() throws Exception {
-//
-//		// Arrange
-//		Category category = new Category(1, "Tartes", true, "Administrator", new Date(), false);
-//		Product product = new Product(1, "Tarte à la framboise", true, 12.0d, false);
-//		category.setProducts(List.of(product));
-//
-//		when(categoryService.getCategoryById(1)).thenReturn(category);
-//
-//		// Execute
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/categories/1");
-//
-//		// Assert
-//		mvc.perform(request).andExpect(status().isForbidden());
-//	}
-//
-//	@Test
-//	@WithMockUser(username = "admin", roles = "ADMIN")
-//	void givenANonExistentCategory_WhenDeleteCategory_ShouldReturnStatusNotFound() throws Exception {
-//
-//		// Arrange
-//		when(categoryService.getCategoryById(1)).thenReturn(null);
-//
-//		// Execute
-//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/categories/1");
-//
-//		// Assert
-//		mvc.perform(request).andExpect(status().isNotFound());
 //	}
 
+
+	@Test
+	@WithMockUser(username = "admin", authorities = "ADMIN")
+	void givenAnNonExistentStaff_whenPatchStatus_returnNotFoundStatus() throws Exception {
+
+		// Arrange
+		when(staffService.getStaffById(2)).thenReturn(null);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/users/2");
+
+		// Execute and Assert
+		mvc.perform(request).andExpect(status().isNotFound());
+	}
+
+//	@Test
+//	@WithMockUser(username = "admin", authorities = "ADMIN")
+//	void givenAStaffId_whenEditName_ReturnUpdatedStaff() throws Exception {
+//
+//		// Arrange
+//		Staff staff = new Staff(1, "Mael", "passWord123!", 0, "ADMIN", true, null, null);
+//		when(staffService.getStaffById(1)).thenReturn(staff);
+//
+//		Staff staffToUpdate = staff;
+//		staffToUpdate.setUsername("MaelLePatron");
+//		when(staffService.saveStaff(staffToUpdate)).thenReturn(staff);
+//		
+//		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/users/1");
+//
+//		// Execute
+//		MockHttpServletResponse response = mvc.perform(request).andReturn().getResponse();
+//
+//		// Assert
+//		String expectedResponse = """
+//						{
+//					    "id": 1,
+//					    "username": "MaelLePatron",
+//					    "role": "ADMIN",
+//					    "status": true
+//						}
+//				""";
+//
+//		assertEquals(mapper.readTree(expectedResponse), mapper.readTree(response.getContentAsString()));
+//	}
+
+	@Test
+	@WithMockUser(username = "admin", authorities = "ADMIN")
+	void givenAStaffWithoutProduct_WhenDeleteStaff_ShouldReturnStatusOk() throws Exception {
+
+		// Arrange
+		Staff staff = new Staff(1, "Mael", "passWord123!", 0, "ADMIN", true, null, null);
+		when(staffService.getStaffById(1)).thenReturn(staff);
+		staff.setStatus(false);
+		when(staffService.updateStaffStatus(1)).thenReturn(staff);
+
+		// Execute
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/users/1");
+
+		// Assert
+		mvc.perform(request).andExpect(status().isOk());
+	}
+
+
+	@Test
+	@WithMockUser(username = "admin", authorities = "ADMIN")
+	void givenANonExistentCategory_WhenDeleteCategory_ShouldReturnStatusNotFound() throws Exception {
+
+		// Arrange
+		when(staffService.getStaffById(1)).thenReturn(null);
+
+		// Execute
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch("/users/1");
+
+		// Assert
+		mvc.perform(request).andExpect(status().isNotFound());
+	}
 
 }
